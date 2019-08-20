8ba4fbd82d421cca9d7f23bd44befdfbc504a38a
Haami Nyangibo <haaminyangibo@gmail.com>
2019-08-20 10:27:46 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,7 +1,7 @@
 # My Code here....
 
-array 
-map_to_negativize([1, 2, 3])
+array = [1, 2. 3]
+map_to_negativize(array)
 map_to_no_change(source_array)
 map_to_double(source_array)
 map_to_square(source_array)
